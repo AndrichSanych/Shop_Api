4f4b1b972958a4c1ef38f1bfb7e42f679db63c06
AndrewSanych <andrich0105@gmail.com>
2024-05-30 09:54:59 +0300

    Add Controllers

---
+++ Shop_Api/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using BusinessLogic.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+
+namespace Shop_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductsService productsService;
+
+        public ProductsController(IProductsService productsService)
+        {
+            this.productsService = productsService;
+        }
+
+        [HttpGet("All")]
+        public IActionResult Get()
+        {
+            return Ok(productsService.GetAll());
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult Get([FromRoute]int id)
+        {
+            return Ok(productsService.Get(id));
+        }
+    }
+}
